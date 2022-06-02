deb3929aea5cee06df98bcbf1e0268a50d97f0bd
Anameskhi <ameskhi@unisens.ge>
2022-06-02 09:24:15 +0400

    sdf

--- db/migrate/20220601085514_add_email_username_password_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddEmailUsernamePasswordToUsers < ActiveRecord::Migration[7.0]
-  def change
-      add_column :users, :username, :varchar
-      add_column :users, :email, :varchar
-      add_column :users, :password, :varchar
-  end
-end
